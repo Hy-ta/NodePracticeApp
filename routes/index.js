4a7bb669e57914839d70e14c9a564ea1fbc8f38e
Hy-ta <hyuuga.1.29.59@gamil.com>
2020-10-08 20:27:48 +0900

    stories model created

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const router = express.Router()
 const { ensureAuth, ensureGuest } = require('../middleware/auth')
 
+const Story = require('../models/Story')
+
 //@desc    Login/Loading page
 //@route   GET /
 router.get('/', ensureGuest, (req, res) => {
@@ -12,8 +14,16 @@ router.get('/', ensureGuest, (req, res) => {
 
 //@desc    Dashboard
 //@route   GET /dashboard
-router.get('/dashboard', ensureAuth, (req, res) => {
-    res.render('dashboard')
+router.get('/dashboard', ensureAuth, async (req, res) => {
+    try {
+        const stories = await Story.find({ user: req.userid }).lean()
+        res.render('dashboard', {
+            name: req.user.firstName,
+            stories
+        })
+    } catch (err){
+        console.error(err)
+    }
 })
 
 
